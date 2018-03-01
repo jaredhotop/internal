713f586f9a4a1da6a23c0a5a1157329e630e4a98
Jayson Scruggs <jayson.scruggs.work@gmail.com>
2018-03-01 16:11:45 -0600

    updated Master Script

--- Crawl/master_script.py
+++ Crawl/master_script.py
@@ -1,13 +1,13 @@
 import os
 import csv
 
-
+os.remove("/media/WebCrawl/unwritten_master.csv")
 
 for file in os.listdir("/media/WebCrawl"):
     if file.endswith(".csv"):
-        with open("unwritten_master.csv","w") as out_f:
-            write = csv.writer(out_f , delimitor=',')
-            with open(file,"r") as in_f:
+        with open("/media/WebCrawl/unwritten_master.csv","w") as out_f:
+            write = csv.writer(out_f,delimiter = ",")
+            with open(os.path.join("/media/WebCrawl/",file),"r") as in_f:
                 read = csv.reader(in_f , delimiter=",")
                 for row in read:
-                    write.writerow(",".join(row))
+                    write.writerow(row)

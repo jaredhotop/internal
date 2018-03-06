424c271c5880bc62f9b018c4d7f8feed76dd897a
Jayson Scruggs <jayson.scruggs.work@gmail.com>
2018-03-05 20:38:42 -0600

    Post merge addendum

--- Crawl/main.py
+++ Crawl/main.py
@@ -10,12 +10,12 @@ import smtplib
 
 try:
     ip = aux_func.get_ip().split(".")
-	file_name = os.path.expanduser("/media/WebCrawl/inputs/tsclinks"+ip[3]+".csv")
+	file_name = os.path.expanduser("/media/WebCrawl/inputs/tsclinks%s.csv" %ip[3])
 	search_arr = written_arr = []
 	with open(file_name,"r" )as f:
 		r = csv.reader(f,delimiter = ",")
 		for row in r:
-			temp = entry_class.Entry(row[0],row[1],row[2],row[3],row[4],row[5],row[6])
+			temp = entry_class.Entry(row[0],row[1],row[2],row[3],row[4],row[5],row[6],ip[3])
 			search_arr.append(temp)
 	#set_cookies()
 	for obj in search_arr:
@@ -23,10 +23,10 @@ try:
 			if obj.get_unique_id == entry.get_unique_id:
 				obj.set_unique_id
 		obj.crawl()
-		obj.write_entry(os.path.expanduser("~/Documents/test_out"+ip[3]+".csv"))
+		obj.write_entry(os.path.expanduser("~/Documents/test_out%s.csv" %ip[3]))
 		written_arr.append(obj)
 		search_arr.pop()
-	os.rename(os.path.expanduser("~/Documents/test_out"+ip[3]+".csv"),os.path.expanduser("/media/WebCrawl/outputs/test_out"+ip[3]+".csv"))
+	os.rename(os.path.expanduser("~/Documents/test_out%s.csv" %ip[3]),os.path.expanduser("/media/WebCrawl/outputs/test_out%s.csv" %ip[3]))
 except:
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()

ddc29f831f2a140dc3c7334c94df8aef3f93a77f
Jayson Scruggs <jayson.scruggs.work@gmail.com>
2018-03-07 20:26:02 -0600

    Logic correction

--- Crawl/master_script.py
+++ Crawl/master_script.py
@@ -4,18 +4,18 @@ import csv
 
 def aggr_uwritten():
     for file in os.listdir("/media/WebCrawl"):
-        if file.endswith(".csv"):
-            with open("/media/WebCrawl/unwritten_master.csv","w") as out_f:
-                write = csv.writer(out_f,delimiter = ",")
+        with open("/media/WebCrawl/unwritten_master.csv","w") as out_f:
+            write = csv.writer(out_f,delimiter = ",")
+            if file.endswith(".csv") and 'master' not in file:
                 with open(os.path.join("/media/WebCrawl/",file),"r") as in_f:
                     read = csv.reader(in_f , delimiter=",")
                     for row in read:
                         write.writerow(row)
 def aggr_valid_records():
     for file in os.listdir("/media/WebCrawl/outputs"):
-        if file.endswith(".csv"):
-            with open("/media/WebCrawl/outputs/valid_records_master.csv","w") as out_f:
-                write = csv.writer(out_f,delimiter = ",")
+        with open("/media/WebCrawl/outputs/valid_records_master.csv","w") as out_f:
+            write = csv.writer(out_f,delimiter = ",")
+            if file.endswith(".csv") and 'master' not in file:
                 with open(os.path.join("/media/WebCrawl/outputs",file),"r") as in_f:
                     read = csv.reader(in_f , delimiter=",")
                     for row in read:

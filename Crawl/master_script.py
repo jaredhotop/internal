3ae4aaa5ed3668fe88188176b10160f800b9ce81
Jayson Scruggs <jayson.scruggs.work@gmail.com>
2018-03-07 21:00:57 -0600

    last time?

--- Crawl/master_script.py
+++ Crawl/master_script.py
@@ -1,25 +1,32 @@
 import os
 import csv
+import Queue
 
+Q = Queue.Queue()
 
-def aggr_uwritten():
-    for file in os.listdir("/media/WebCrawl"):
-        with open("/media/WebCrawl/unwritten_master.csv","w") as out_f:
-            write = csv.writer(out_f,delimiter = ",")
-            if file.endswith(".csv") and 'master' not in file:
-                with open(os.path.join("/media/WebCrawl/",file),"r") as in_f:
-                    read = csv.reader(in_f , delimiter=",")
-                    for row in read:
-                        write.writerow(row)
+ def aggr_uwritten():
+     for file in os.listdir("/media/WebCrawl"):
+     if file.endswith(".csv") and 'master' not in file:
+        with open(os.path.join("/media/WebCrawl/",file),"r") as in_f:
+            read = csv.reader(in_f , delimiter=",")
+            for row in read:
+                Q.put(row)
+    with open("/media/WebCrawl/unwritten_master.csv","w") as out_f:
+        write = csv.writer(out_f,delimiter = ",")
+        while not Q.empty():
+            write.writerow(Q.get())
+
 def aggr_valid_records():
     for file in os.listdir("/media/WebCrawl/outputs"):
-        with open("/media/WebCrawl/outputs/valid_records_master.csv","w") as out_f:
-            write = csv.writer(out_f,delimiter = ",")
-            if file.endswith(".csv") and 'master' not in file:
-                with open(os.path.join("/media/WebCrawl/outputs",file),"r") as in_f:
-                    read = csv.reader(in_f , delimiter=",")
-                    for row in read:
-                        write.writerow(row)
+         if file.endswith(".csv") and 'master' not in file:
+            with open(os.path.join("/media/WebCrawl/outputs",file),"r") as in_f:
+                read = csv.reader(in_f , delimiter=",")
+                for row in read:
+                    Q.put(row)
+    with open("/media/WebCrawl/outputs/valid_records_master.csv","w") as out_f:
+        write = csv.writer(out_f,delimiter = ",")
+        while not Q.empty():
+            write.writerow(Q.get())
 
 aggr_uwritten()
 aggr_valid_records()

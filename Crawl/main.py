12ae77d60354a43cd457030f8c1aac0eef94da09
Jayson Scruggs <jayson.scruggs.work@gmail.com>
2018-03-12 09:01:37 -0500

    quick fix

--- Crawl/main.py
+++ Crawl/main.py
@@ -23,7 +23,7 @@ try:
     with open(file_name,"r" )as f:
         r = csv.reader(f,delimiter = ",")
         for row in r:
-            temp = entry_class_logger.Entry(row[0],row[1],row[2],row[3],row[4],row[5],row[6],ip[3])
+            temp = entry_class.Entry(row[0],row[1],row[2],row[3],row[4],row[5],row[6],ip[3])
             search_arr.append(temp)
 	written_arr = []
     while search_arr:

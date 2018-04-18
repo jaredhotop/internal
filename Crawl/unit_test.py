62c6f91d6cea116723bdb734659278e49cfe9e8a
Jayson Scruggs <jayson.scruggs.work@gmail.com>
2018-04-18 18:55:30 -0500

    unit testing

--- Crawl/unit_test.py
+++ Crawl/unit_test.py
@@ -1,5 +1,6 @@
 import unittest
-import main.py
+import entry_class
+import stores
 
 class LinkTest(unittest.TestCase):
 
@@ -7,6 +8,7 @@ class LinkTest(unittest.TestCase):
         obj = entry_class.Entry('7','1234','4665',False,False,'1',\
         'https://www.menards.com/main/building-materials/panel-products/construction-panels/sheathing/osb-sheathing/4-x-8-osb/p-1444422395209-c-13330.htm','90')
         obj.crawl()
+        print(obj)
         self.assertEqual('16.25',obj.comp_price)
         print('testing')
 
